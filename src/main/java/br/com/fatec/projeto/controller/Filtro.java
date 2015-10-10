55779267012fd23019b0df5c548f03126f662cb3
karenyov <karenyasmin37@gmail.com>
2015-10-10 15:55:50 -0300

    Filtro

---
+++ src/main/java/br/com/fatec/projeto/controller/Filtro.java
@@ -0,0 +1,66 @@
+package br.com.fatec.projeto.controller;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet Filter implementation class Filtro
+ */
+@WebFilter(urlPatterns = { "/editUser", "" })
+public class Filtro implements Filter {
+
+	/**
+	 * Default constructor.
+	 */
+	public Filtro() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		// place your code here
+
+		// pass the request along the filter chain
+
+		HttpServletRequest ht = (HttpServletRequest) request;
+
+		HttpSession session = ht.getSession();
+
+		Object user = session.getAttribute("usuarioLogado");
+
+		if (user == null) {
+			ht.getRequestDispatcher("login.jsp").forward(request, response);
+		} else {
+			chain.doFilter(request, response);
+		}
+
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
